bbdc6dc6f83e0b859f261fda35fe06801d0432a0
Seon82 <46298009+Seon82@users.noreply.github.com>
2023-01-13 13:19:51 +0100

    perf: accelerate bike lane per commune computation

--- velosafe/data/bike_lane_processing.py
+++ velosafe/data/bike_lane_processing.py
@@ -8,6 +8,27 @@ from shapely.geometry import LineString, MultiLineString, Point, Polygon, mappin
 from tqdm import tqdm
 
 
+def fast_compute_bike_lane_length_per_commune(df_bike_lanes, df_communes, epsg=27561):
+    ## Compute intersection
+    # Join cycling lanes to communes if they intersect
+    intersections = gpd.sjoin(df_bike_lanes, df_communes, how="inner", predicate="intersects")
+
+    # Re-add commune geometry, lost in the sjoin
+    combined = gpd.GeoDataFrame(intersections.merge(df_communes, on="insee_com", suffixes=("_cycling", "_insee")))
+
+    # Compute line intersection between cycling lanes and communes
+    cycling_lanes_intersected = combined.geometry_cycling.intersection(combined.geometry_insee)
+
+    ## Compute length
+    cycling_lanes_intersected = cycling_lanes_intersected.to_frame("lane")
+    cycling_lanes_intersected["insee_com"] = combined.insee_com
+
+    # Project to planar coordinate system
+    cycling_lanes_intersected["length"] = cycling_lanes_intersected.lane.to_crs(epsg=epsg).length
+
+    return cycling_lanes_intersected
+
+
 def compute_bike_lane_length_per_commune(df_bike_lanes, df_communes):
     # we merge all the lines representing the bike lanes to obtain one multilines object
     # containing all the bike lanes in France
@@ -45,8 +66,9 @@ def compute_bike_lane_length_per_commune(df_bike_lanes, df_communes):
 if __name__ == "__main__":
     df_bike_lanes = gpd.read_file("./../france.geojson")
     df_communes = gpd.read_file("./../code-postal-code-insee-2015.geojson")
-
+    print("Loaded data")
     sample_bike_lanes = df_bike_lanes
     sample_communes = df_communes
 
-    compute_bike_lane_length_per_commune(sample_bike_lanes, sample_communes)
+    cycling_lane_length = fast_compute_bike_lane_length_per_commune(sample_bike_lanes, sample_communes)
+    print(cycling_lane_length)
