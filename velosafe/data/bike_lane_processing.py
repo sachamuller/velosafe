a5aa7055d50bf61a8c170b542ae43f62f9ece4db
Sacha Muller <chloe.muller@student-cs.fr>
2023-01-18 09:33:53 +0100

    cleaned bike lane processing file

--- velosafe/data/bike_lane_processing.py
+++ velosafe/data/bike_lane_processing.py
@@ -1,14 +1,17 @@
-import itertools
-
 import geopandas as gpd
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-from shapely.geometry import LineString, MultiLineString, Point, Polygon, mapping
-from tqdm import tqdm
 
 
-def fast_compute_bike_lane_length_per_commune(df_bike_lanes, df_communes, epsg=27561):
+def fast_compute_bike_lane_length_per_commune(df_bike_lanes:gpd.GeoDataFrame, df_communes:gpd.GeoDataFrame, epsg:int=27561)-> gpd.GeoDataFrame:
+    """_summary_
+
+    Args:
+        df_bike_lanes (gpd.GeoDataFrame): geopandas dataframe containing the geometry of all bike lanes in france
+        df_communes (gpd.GeoDataFrame): geopandas dataframe containing the shape of french communes 
+        epsg (int, optional): EPSG code specifying output projection. Defaults to 27561.
+
+    Returns:
+        gpd.GeoDataFrame: the communes dataframe with a new column containing the total length of bike lanes in each commune
+    """
     ## Compute intersection
     # Join cycling lanes to communes if they intersect
     intersections = gpd.sjoin(df_bike_lanes, df_communes, how="inner", predicate="intersects")
@@ -25,50 +28,18 @@ def fast_compute_bike_lane_length_per_commune(df_bike_lanes, df_communes, epsg=2
 
     # Project to planar coordinate system
     cycling_lanes_intersected["length"] = cycling_lanes_intersected.lane.to_crs(epsg=epsg).length
-
     return cycling_lanes_intersected
 
 
-def compute_bike_lane_length_per_commune(df_bike_lanes, df_communes):
-    # we merge all the lines representing the bike lanes to obtain one multilines object
-    # containing all the bike lanes in France
-    bike_lanes_multilines_df = df_bike_lanes["geometry"]
-
-    print(bike_lanes_multilines_df)
-
-    bike_lanes_lines_df = bike_lanes_multilines_df.apply(list)  # convert multilines in list of lines
-    print("df apply list")
-    list_bike_lanes_lines = list(bike_lanes_lines_df)
-    print("convert df to list")
-    # list_bike_lanes_lines = sum(list_bike_lanes_lines, [])  # flatten list of list
-    list_bike_lanes_lines = list(itertools.chain.from_iterable(list_bike_lanes_lines))
-    print("flatten")
-
-    all_bike_lanes = MultiLineString(list_bike_lanes_lines)
-    print("created multistring")
-
-    # compute intersection with communes polygons
-    df_communes["bike lanes total length"] = None
-    for i in tqdm(list(df_communes.index)):
-        polygon = df_communes.loc[i, "geometry"]
-        df_communes.loc[i, "bike lanes total length"] = polygon.intersection(all_bike_lanes).length
-        if i % 100 == 0:
-            df_communes.to_csv("communes_with_bike_length_intermediate.csv")
-
-    # df_communes["bike lanes total length"] = df_communes["geometry"].apply(
-    #     lambda x: x.intersection(all_bike_lanes).length
-    # )
-    print("added length to commune")
-
-    df_communes.to_csv("communes_with_bike_length.csv")
 
 
 if __name__ == "__main__":
-    df_bike_lanes = gpd.read_file("./../france.geojson")
-    df_communes = gpd.read_file("./../code-postal-code-insee-2015.geojson")
+    df_bike_lanes = gpd.read_file("data/france.geojson")
+    df_communes = gpd.read_file("data/code-postal-code-insee-2015.geojson")
     print("Loaded data")
     sample_bike_lanes = df_bike_lanes
     sample_communes = df_communes
 
     cycling_lane_length = fast_compute_bike_lane_length_per_commune(sample_bike_lanes, sample_communes)
     print(cycling_lane_length)
+    df_communes.to_csv("data/communes_with_bike_length.csv")
